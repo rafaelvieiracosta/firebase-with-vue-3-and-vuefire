09d279a8801e23890ef68fbb4e0735f58df305b6
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-01-01 19:55:55 -0300

    feat: registering users with firebase auth

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { VueFire } from 'vuefire'
+import { VueFire, VueFireAuth } from 'vuefire'
 import { firebaseApp } from '@/firebase.js'
 
 import App from './App.vue'
@@ -21,7 +21,7 @@ const app = createApp(App)
 
 app.use(VueFire, {
   firebaseApp,
-  modules: [],
+  modules: [VueFireAuth],
 })
 app.use(router)
 app.use(vuetify)
